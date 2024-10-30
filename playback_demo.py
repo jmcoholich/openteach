961618b3084c669d6b040a23e3638c0064a0cb2f
Jeremiah <jcoholich@gatech.edu>
2024-10-30 12:31:33 -0400

    switch to just using the classes from openteachcontrollers, much cleaner now

--- playback_demo.py
+++ playback_demo.py
@@ -1,3 +1,14 @@
+"""
+This script is for replaying demonstrations from .pkl files and from the RLDS files
+for verification and debugging purposes.
+
+In order to run this script, you need to have the following running on the NUC
+
+cd deoxys_control/deoxys && ./auto_scripts/auto_arm.sh config/charmander.yml
+cd deoxys_control/deoxys && ./auto_scripts/auto_gripper.sh config/charmander.yml
+
+"""
+
 # deoxys_control
 from deoxys.franka_interface import FrankaInterface
 import deoxys.proto.franka_interface.franka_controller_pb2 as franka_controller_pb2
@@ -24,8 +35,7 @@ import os
 
 # add openteachcontrollers to path
 sys.path.append("/home/ripl/openteachcontrollers/src/franka-arm-controllers/franka_arm")
-from constants import *
-from utils.config import get_velocity_controller_config
+from controller import FrankaController
 
 parser = argparse.ArgumentParser()
 parser.add_argument("demo", type=str, help="The name of the demonstration to visualize")
@@ -121,120 +131,29 @@ def dataloader(args):
                 # sleep(1)
 
 def main(args):
-    # Initialize robot
-    logger = get_deoxys_example_logger()  # logger for debugging
-    # reference code at /home/ripl/openteachcontrollers/src/franka-arm-controllers/franka_arm/controller.py
-    # get home dir
-    home = os.path.expanduser("~")
-    config_path = os.path.join(
-        home,
-        "deoxys_control/deoxys/config/charmander.yml")
-    robot_interface = FrankaInterface(
-        config_path,
-        use_visualizer=False,
-        control_freq=15,
-        state_freq=STATE_FREQ,
-        )
-    # controller_type = "OSC_POSE"  # controls end effector in 6 dimensions, need to use serpeate controller for gripper
-    # controller_cfg = get_default_controller_config(controller_type=controller_type)
+    franka_controller = FrankaController(record=False, control_freq=15)
 
-    velocity_controller_cfg = get_velocity_controller_config(
-            config_root = CONFIG_ROOT
-        )
+    home = os.path.expanduser("~")
     # Load demonstration data
     filename = f"{home}/openteach/extracted_data/pick_coke/demonstration_{args.demo}/demo_{args.demo}.pkl"
     # arm_cmd_file = f"/home/ripl/openteach/extracted_data/pick_coke/demonstration_coke18/franka_arm_tcp_commands.h5"
     with open(filename, 'rb') as dbfile:
         db = pkl.load(dbfile)
 
-    # with h5py.File(arm_cmd_file, "r") as f:
-    #     tpc_cmd = f['arm_tcp_commands'][()]
-
     # binarize gripper actions (-1, 1)
     gripper_actions = np.where(db['gripper_cmd'] <= 0, 1, -1)
     assert len(gripper_actions) == len(db['eef_pose'])
 
     # move robot to start position
-    reset_joints_to(robot_interface, db['joint_angles'][0])
+    reset_joints_to(franka_controller.robot_interface, db['joint_angles'][0])
 
     for i in range(len(gripper_actions)):
-        # skip starting position
-        N = 1  # number of steps to skip
-        if (i == 0
-           or i+N+1 >= len(gripper_actions)
-           or i%N != 0):
-            continue
-
-        # can replay demos well when we use the robots actual current postion to calulate deltas
-        # current_pos, current_quat = (robot_interface.last_eef_quat_and_pos[1].flatten(), robot_interface.last_eef_quat_and_pos[0])
-
-        # when we use the robots recorded position to calculate deltas, the accured error is noticable but this is more accurate to how we are training
-        current_pos = db['eef_pose'][i][:3, 3]
-        current_quat = mat2quat(db['eef_pose'][i][:3, :3])
-
-        # get the goal position
-        target_pos = db['eef_pose'][i+N][:3, 3]
-
-        # calculate the deltas needed to go from current to target position
-        delta_pos = target_pos - current_pos
-        target_quat = mat2quat(db['eef_pose'][i+N][:3, :3])
-        if np.dot(target_quat, current_quat) < 0.0:
-            current_quat = -current_quat
-        delta_rpy = quat2axisangle(target_quat) - quat2axisangle(current_quat)
-
-        deltas = np.concatenate((delta_pos, delta_rpy))
-
-        # deltas = tpc_cmd[i]  # use commanded deltas instead of calculated deltas
-
-        print(f"Calculated: {deltas}\n")
-        # move_to_target_pose(
-        #             robot_interface,
-        #             controller_type,
-        #             controller_cfg,
-        #             target_delta_pose=deltas,
-        #             num_steps=10,
-        #             num_additional_steps=1,
-        #             interpolation_method="linear"
-        #             )
-        cartesian_control(robot_interface, velocity_controller_cfg, np.concatenate((target_pos, target_quat)))
-        # need to be careful how we actually calculate the deltas here when diretly publishing to the robot
-        # deltas[:3] = [i/.05 for i in deltas[:3]]  # counter action scale so the deltas that are pblished to robot are the same as calculated here
-        # robot_interface.control(
-        #     controller_type=controller_type,
-        #     action=deltas,
-        #     controller_cfg=controller_cfg,
-        # )
-        robot_interface.gripper_control(gripper_actions[i])
-
-
-def cartesian_control(robot_interface, velocity_controller_cfg, cartesian_pose): # cartesian_pose: (7,) (pos:quat) - pos (3,) translational pose, quat (4,) quaternion
-    # copied from /home/ripl/openteachcontrollers/src/franka-arm-controllers/franka_arm/controller.py
-    cartesian_pose = np.array(cartesian_pose, dtype=np.float32)
-    target_pos, target_quat = cartesian_pose[:3], cartesian_pose[3:]
-    target_mat = transform_utils.pose2mat(pose=(target_pos, target_quat))
-
-    current_quat, current_pos = robot_interface.last_eef_quat_and_pos
-    current_mat = transform_utils.pose2mat(pose=(current_pos.flatten(), current_quat.flatten()))
-
-    pose_error = transform_utils.get_pose_error(target_pose=target_mat, current_pose=current_mat)
-
-    if np.dot(target_quat, current_quat) < 0.0:
-        current_quat = -current_quat
-    quat_diff = transform_utils.quat_distance(target_quat, current_quat)
-    axis_angle_diff = transform_utils.quat2axisangle(quat_diff)
-
-    action_pos = pose_error[:3] * TRANSLATIONAL_POSE_VELOCITY_SCALE
-    action_axis_angle = axis_angle_diff.flatten() * ROTATIONAL_POSE_VELOCITY_SCALE
-
-    action_pos, _ = transform_utils.clip_translation(action_pos, TRANSLATION_VELOCITY_LIMIT)
-    action_axis_angle = np.clip(action_axis_angle, -ROTATION_VELOCITY_LIMIT, ROTATION_VELOCITY_LIMIT)
-    action = action_pos.tolist() + action_axis_angle.tolist()
-
-    robot_interface.control(
-        controller_type=CONTROLLER_TYPE,
-        action=action,
-        controller_cfg=velocity_controller_cfg,
-    )
+        cartesian_pose = np.concatenate((
+            db['eef_pos'][i],
+            db['eef_quat'][i]
+        ))
+        franka_controller.cartesian_control(cartesian_pose)
+        franka_controller.set_gripper_position(gripper_actions[i])
 
 if __name__ == "__main__":
     main(parser.parse_args())

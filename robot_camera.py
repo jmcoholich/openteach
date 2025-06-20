d5e8e2063708795071340c9cff92d3a8a206a11f
JustinWit <jwit3@gatech.edu>
2025-06-20 14:14:03 -0400

    reset cams on start up

--- robot_camera.py
+++ robot_camera.py
@@ -1,6 +1,20 @@
 import hydra
 from openteach.components import RealsenseCameras
 import time
+# https://github.com/IntelRealSense/librealsense/issues/6628#issuecomment-646558144
+import pyrealsense2 as rs
+
+# initiating hardware reset
+ctx = rs.context()
+devices = ctx.query_devices()
+print(devices)
+for dev in devices:
+    print("resetting device: ", dev.get_info(rs.camera_info.serial_number))
+    dev.hardware_reset()
+    print(" done")
+
+import time; time.sleep(2)
+
 
 @hydra.main(version_base = '1.2', config_path = 'configs', config_name = 'camera')
 def main(configs):

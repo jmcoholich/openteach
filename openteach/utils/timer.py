16c18bb5903b0ee0c058493beed5c6d0d98eb202
Jeremiah Coholich <jcoholich@gatech.edu>
2026-03-04 09:16:09 -0500

    solve stale frames issue by switching back to ZMQ Conflate and just doing PNG instead of raw bytes

--- openteach/utils/timer.py
+++ openteach/utils/timer.py
@@ -1,13 +1,9 @@
 import pickle
-import struct
 import time
 
 import cv2
 import numpy as np
 import zmq
-
-RGB_HEADER_FORMAT = '!dIIIB'
-RGB_DTYPE_UINT8 = 1
 
 
 class FrequencyTimer(object):
@@ -35,8 +31,7 @@ class SocketChecker(object):
     def _init_connection(self, host, port):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.SUB)
-        if self.data_type == 'FloatArray':
-            self.socket.setsockopt(zmq.CONFLATE, 1)
+        self.socket.setsockopt(zmq.CONFLATE, 1)
         self.socket.setsockopt(zmq.SUBSCRIBE, bytes(self.topic_name, 'utf-8'))
         self.socket.connect('tcp://{}:{}'.format(host, port))
 
@@ -54,33 +49,16 @@ class SocketChecker(object):
         print(pickle.loads(processed_data))
 
     def _decode_rgb_image(self):
-        if not (isinstance(self.data, list) and len(self.data) == 3 and self.data[0] == b"rgb_image"):
-            raise ValueError('Unexpected rgb_image message format.')
-
-        _, height, width, channels, dtype_code = struct.unpack(RGB_HEADER_FORMAT, self.data[1])
-        if dtype_code != RGB_DTYPE_UINT8:
-            raise ValueError('Unsupported rgb_image dtype code: {}'.format(dtype_code))
-
-        frame = np.frombuffer(self.data[2], dtype = np.uint8)
-        expected_values = height * width * channels
-        if frame.size != expected_values:
-            raise ValueError(
-                'Invalid rgb_image payload size: expected {}, got {}.'.format(expected_values, frame.size)
-            )
-        if channels == 1:
-            image = frame.reshape((height, width))
-        else:
-            image = frame.reshape((height, width, channels))
+        frame = pickle.loads(self.data[len(b"rgb_image "):])
+        encoded_data = np.frombuffer(frame['rgb_image'], np.uint8)
+        image = cv2.imdecode(encoded_data, cv2.IMREAD_COLOR)
         cv2.imshow('rgb_image', image)
         cv2.waitKey(1)
 
     def check_connection(self):
         self._reinit_counter()
         while True:
-            if self.data_type == 'FloatArray':
-                self.data = self.socket.recv()
-            else:
-                self.data = self.socket.recv_multipart()
+            self.data = self.socket.recv()
             if self.data is not None and self.data is not self.previous_data:
                 # To see the data - usually reduces the actual frequency. Use it to just see the stream
                 if self.print_data:
--- server/application_manager.py
+++ server/application_manager.py
@@ -1,11 +1,6 @@
-import struct
+import pickle
 
-import cv2
-import numpy as np
 import zmq
-
-RGB_HEADER_FORMAT = '!dIIIB'
-RGB_DTYPE_UINT8 = 1
 
 
 class VideoStreamer(object):
@@ -15,37 +10,19 @@ class VideoStreamer(object):
     def _init_socket(self, host, port):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.SUB)
-        # ZMQ_CONFLATE is not safe with multipart messages.
+        self.socket.setsockopt(zmq.CONFLATE, 1)
         self.socket.connect('tcp://{}:{}'.format(host, port))
         self.socket.setsockopt(zmq.SUBSCRIBE, b"rgb_image")
 
     def _get_image(self):
-        parts = self.socket.recv_multipart()
-
-        if not (len(parts) == 3 and parts[0] == b"rgb_image"):
-            raise ValueError('Unexpected rgb_image message format.')
-
-        _, height, width, channels, dtype_code = struct.unpack(RGB_HEADER_FORMAT, parts[1])
-        if dtype_code != RGB_DTYPE_UINT8:
-            raise ValueError('Unsupported rgb_image dtype code: {}'.format(dtype_code))
-
-        frame = np.frombuffer(parts[2], dtype = np.uint8)
-        expected_values = height * width * channels
-        if frame.size != expected_values:
-            raise ValueError(
-                'Invalid rgb_image payload size: expected {}, got {}.'.format(expected_values, frame.size)
-            )
-        if channels == 1:
-            image = frame.reshape((height, width))
-        else:
-            image = frame.reshape((height, width, channels))
-        _, encoded_data = cv2.imencode('.jpg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 70])
-        return encoded_data.tobytes()
+        raw_data = self.socket.recv()
+        data = pickle.loads(raw_data[len(b"rgb_image "):])
+        return data['rgb_image']
 
     def yield_frames(self):
         while True:
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + self._get_image() + b'\r\n')  # concat frame one by one and show result
+                   b'Content-Type: image/png\r\n\r\n' + self._get_image() + b'\r\n')  # concat frame one by one and show result
 
 
 class MonitoringApplication(object):
